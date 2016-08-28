f2f5b02f4e4984ba9f992e351be50bf64e992d40
Lauren Yockey <laurenyockey@gmail.com>
2016-08-28 11:41:38 -0500

    Added Skeleton framework and placeholder content

--- index.php
+++ index.php
@@ -3,7 +3,39 @@
 <head>
   <meta charset="UTF-8">
   <title>Lauren Yockey Blank WordPress Template</title>
+  <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
 </head>
+
 <body>
+  <div class="container">
+    <header class="row">
+      <div class="twelve columns">
+        <h1>Blank WordPress Template</h1>
+        <p>This is my WordPress template.</p>
+      </div>
+    </header>
+
+    <section class="row">
+      <div class="twelve columns">
+        <h2>Section Content</h2>
+        <p>This is placeholder text for the Section Content area.</p>
+      </div>
+    </section>
+
+    <footer class="row">
+      <div class="twelve columns">
+        <h2>This is the Footer.</h2>
+        <p>This is placeholder text for the Footer area.</p>
+      </div>
+    </footer>
+  </div>
+
+
+
+
+
+
+
+
 </body>
 </html>
