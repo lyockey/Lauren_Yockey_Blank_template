6c2f308571a9f4e8815051c2c750e09747a405b6
Lauren Yockey <laurenyockey@gmail.com>
2016-09-19 12:50:27 -0500

    add widget to function.php

---
+++ functions.php
@@ -0,0 +1,18 @@
+<?php
+/*-------------- Enable Widgets--------------- */
+
+function blank_widgets_init() {
+    register_sidebar( array(
+        'name' => ('First Widget'),
+        'id' => 'first-widget',
+        'description' => 'Widget for our sidebar on pages',
+        'before_widget' => '<div class="widget-sidebar">',
+        'after_widget' => '</div>',
+        'before_title' => '<h2>',
+        'after_title' => '</h2>'
+    ));
+}
+
+add_action('widgets_init', 'blank_widgets_init');
+
+?>
