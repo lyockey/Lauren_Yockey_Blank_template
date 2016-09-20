323579a83b9f3461c424ac3ea1f0d938421458e8
Lauren Yockey <laurenyockey@gmail.com>
2016-09-20 12:35:03 -0500

    add menu functionality

--- functions.php
+++ functions.php
@@ -50,3 +50,6 @@ function blank_widgets_init() {
 }
 
 add_action('widgets_init', 'blank_widgets_init');
+
+/*-------------- Enable Menu --------------- */
+add_theme_support('menus');
--- header.php
+++ header.php
@@ -19,4 +19,15 @@
         <h2><?php bloginfo( 'description'); ?></h2>
       </div>
     </header>
+
+    <div class="row">
+        <div class="twelve columns">
+            <?php
+                wp_nav_menu( array(
+                    'sort_column'      =>  'menu_order',
+                    'container_class'  =>  'blank-menu-header'
+                ));
+            ?>
+        </div>
+    </div>
 <!-- End Header -->
